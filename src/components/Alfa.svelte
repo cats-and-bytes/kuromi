a819d3bca6587a2bdbd18ecf11323f4cd8fc2c45
Bitocas <kai@pcr.com>
2025-08-02 18:19:41 -0400

    Alfa component.

---
+++ src/components/Alfa.svelte
@@ -0,0 +1,8 @@
+<div>Loreninha</div>
+
+<style>
+  div {
+    color: green;
+    font-size: 5rem;
+  }
+</style>
---
+++ src/routes/a/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+  import Alfa from "../../components/Alfa.svelte";
+</script>
+<Alfa> </Alfa>
